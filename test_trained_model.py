0d310a2602de5d13b01a40f33e7b7feae9037b1e
LemsE <60966044+LemsE@users.noreply.github.com>
2023-09-13 13:24:26 +0200

    Update test_trained_model.py

--- test_trained_model.py
+++ test_trained_model.py
@@ -16,10 +16,78 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import pandas as pd
 import train_and_test as tnt
-
+torch.manual_seed(42)
 from sklearn.metrics import top_k_accuracy_score, precision_score, recall_score, f1_score
-import argparse
-import os
+
+import random
+np.random.seed(42)
+random.seed(42)
+torch.manual_seed(42)
+"""
+Code for testing intermediate checkpoints
+
+"""
+
+def test(
+        loaded_model, device, 
+        dataloaders, img_sets
+        ) -> None:
+    """
+    Tests a PyTorch model. 
+
+    Parameters
+    -----------
+        loaded_model : torchvision.models.ResNet
+            Trained and loaded ResNet model
+        device : torch.device
+            Device to be utilized
+        dataloaders : Dataloader
+            Dataloader used for loading the data onto the model
+        test_set : datasets.ImageFolder
+            Test set of the data
+    
+    """
+    start = timeit.default_timer()
+    loaded_model.eval()
+    with torch.no_grad():
+        n_correct = 0
+        for inputs,labels in tqdm(dataloaders['test']):
+            inputs = inputs.to(device=device)
+            labels = labels.to(device=device)
+
+            output, min_dist = loaded_model(inputs)
+            __, predicted = torch.max(output.data, 1)
+            
+            n_correct += torch.sum(predicted == labels.data)
+        accuracy = n_correct/len(dataloaders['test'].dataset)
+        print('Test accuracy: {:.4f} '.format(accuracy))
+    stop = timeit.default_timer()
+    time = stop - start
+    print('Test time in s: {} '.format(time) )
+
+def get_misclassified(loaded_model, device, 
+        dataloaders):
+    loaded_model.eval()
+    with torch.no_grad():
+        n_correct = 0
+        mis_samples = []
+        for inputs,labels in tqdm(dataloaders['test']):
+            inputs = inputs.to(device=device)
+            labels = labels.to(device=device)
+
+            output, min_dist = loaded_model(inputs)
+            __, predicted = torch.max(output.data, 1)
+
+            mask = (predicted!=labels)
+            missclassified_img = inputs[mask]
+            missclassified_lab = labels[mask]
+            mis_samples.append(missclassified_img)
+
+            n_correct += torch.sum(predicted == labels.data)
+
+        accuracy = n_correct/len(dataloaders['test'].dataset)
+        print('Test accuracy: {:.4f} '.format(accuracy))
+        print(mis_samples)
 
 def conf_matrix(loaded_model, device, 
         dataloaders):
@@ -30,9 +98,9 @@ def conf_matrix(loaded_model, device,
             true_labels = []
             pred_labels = []
             logits = []
-
+            # save_labels = None
             for inputs,labels in tqdm(dataloaders['test']):
-
+                # save_labels = labels.
                 inputs = inputs.to(device=device)
                 labels = labels.to(device=device)
 
@@ -64,24 +132,36 @@ def conf_matrix(loaded_model, device,
             cm_display = metrics.ConfusionMatrixDisplay(confusion_mat, display_labels=labels_plot)
             cm_display.plot()
             plt.show()
-            #plt.savefig('confmatrix_nowarmup_epoch47.png'.format())
+            
             df = pd.DataFrame(confusion_matrix, index=true_labels, columns=true_labels)
-            #df.to_csv('confusion_matrix_001_epoch20.csv', sep=',')
-            # print(mis_samples)
 
 
+def test_conv_features(loaded_model, device, dataloaders):
+    start = timeit.default_timer()
+    loaded_model.eval()
+    with torch.no_grad():
+        n_correct = 0
+        for inputs,labels in tqdm(dataloaders['test']):
+            inputs = inputs.to(device=device)
+            labels = labels.to(device=device)
+
+            x = loaded_model.module.conv_features(inputs)
+            print(x.size())
+
+    stop = timeit.default_timer()
+    time = stop - start
+    print('Test time in s: {} '.format(time) )
+
+
+
+
+
+        
 def main():
-    # Just for testing
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-gpuid', nargs=1, type=str, default='5') # python3 main.py -gpuid=0,1,2,3
-    # args = parser.parse_args()
-    # os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuid[0]
-    # print(os.environ['CUDA_VISIBLE_DEVICES'])
-    
     dataloaders, img_sets = create_dataloaders()
-    
-    load_model_path = '/home/elilem/project_xai/XAI_feedbackloop/saved_models/resnet34/beta_05/15nopush0.8777.pth' 
+
+    load_model_path = './models/beta09/seeds/24_9end0.8272.pth' 
     ppnet = model.construct_PPNet(base_architecture=base_architecture,
                                 pretrained=True, img_size=img_size,
                                 prototype_shape=prototype_shape,
@@ -89,14 +169,19 @@ def main():
                                 prototype_activation_function=prototype_activation_function,
                                 add_on_layers_type=add_on_layers_type)
     
-   
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    ppnet = torch.load(load_model_path)
-    ppnet = ppnet.to(device)
-    ppnet_multi = torch.nn.DataParallel(ppnet)
+
     
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ppnet = torch.load(load_model_path)
+    ppnet = ppnet.cuda()
+    ppnet_multi = torch.nn.DataParallel(ppnet)
+
+    
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     conf_matrix(ppnet_multi, device, dataloaders)
+    #test(loaded_model=ppnet_multi,device=device,dataloaders=dataloaders, img_sets=img_sets)
+
+
 
 if __name__ == '__main__':
     main()
